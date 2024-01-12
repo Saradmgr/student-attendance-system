f2f61e7861c4490f292f3871e618a6e08c4220df
Saradmgr <saradkunwor20@gmail.com>
2024-01-12 12:06:18 +0545

    login form adjust

--- login_form.php
+++ login_form.php
@@ -20,7 +20,7 @@ if (isset($_POST['submit'])) {
       if ($row['user_type'] == 'admin') {
 
          $_SESSION['admin_name'] = $row['name'];
-         header('location:homepage.php');
+         header('location:dashboardstd.html');
       } elseif ($row['user_type'] == 'user') {
 
          $_SESSION['user_name'] = $row['name'];
